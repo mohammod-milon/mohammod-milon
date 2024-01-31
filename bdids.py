b01df385da7ff52a51bff82ee47adc53fedd666b
mohammod-milon <112416603+mohammod-milon@users.noreply.github.com>
2024-01-31 21:53:29 +0600

    Create bdids.py
    Id clone

---
+++ bdids.py
@@ -0,0 +1,9 @@
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/mohammod-milon/bdids.py.git
+cd bdids.py
+Python2 bdid.py
